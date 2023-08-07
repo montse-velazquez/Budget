509df5aba7d8b3d3eae39717425885f18268bd86
montse-velazquez <12337@coderacademy.edu.au>
2023-08-07 22:32:39 +1000

    Create NavBar Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,15 @@
 import { NavLink } from "react-router-dom";
+import './Navbar.scss'
 
 export function Navbar(props) {
 
-    let activeNavStyle ={
-        textDecorationColor: "green"
-    }
-
     return(
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/goals" style={({isActive}) => isActive ? activeNavStyle : undefined} >Goals</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/expenses" style={({isActive}) => isActive ? activeNavStyle : undefined} >Expenses</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/bills" style={({isActive}) => isActive ? activeNavStyle : undefined} >Bills</NavLink>
-                </li>
-            </ul>
+        <nav className="navbar">
+                    <NavLink to="/goals" >Goals</NavLink>
+                    <NavLink to="/expenses" >Expenses</NavLink>
+                    <p>Budget Manager</p>
+                    <NavLink to="/bills" >Bills</NavLink>
+                    <NavLink>Money In/Out</NavLink>
         </nav>
     )
 }
